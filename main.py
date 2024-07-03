c253b5b3bbd98bd63d64b8848378509e5a78109b
Gonzalo Melo <gonzalomelov@gmail.com>
2024-07-03 18:54:38 -0300

    chore: add debugging prints to main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from dotenv import load_dotenv
 from db_config import get_postgres_connection, get_mysql_connection
@@ -15,10 +16,16 @@ conn_mysql, cur_mysql = get_mysql_connection()
 attestations_csv = pd.read_csv('attestations.csv')
 
 # Create user profiles
+create_user_profiles_start_time = time.time()
 users = create_user_profiles(cur_pg, attestations_csv)
+create_user_profiles_end_time = time.time()
+print(f"Total time to run create_user_profiles: {create_user_profiles_end_time - create_user_profiles_start_time:.2f} seconds")
 
 # Get products and recommend
+recommend_products_start_time = time.time()
 product_data = recommend_products(cur_mysql, conn_mysql, users)
+recommend_products_end_time = time.time()
+print(f"Total time to run recommend_products: {recommend_products_end_time - recommend_products_start_time:.2f} seconds")
 
 # Clean up database connections
 cur_pg.close()
